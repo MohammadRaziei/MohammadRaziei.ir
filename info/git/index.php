18291930430a36f0d5ff5315aa6579f80f246afa
mohammadraziei <mohammadraziei1375@gmail.com>
2019-09-27 00:25:25 +0330

    hi

---
+++ info/git/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// PHP permanent URL redirection test
+
+// header("Location: fa", true, 301);
+
+header("Location: https://github.com/MohammadRaziei/MohammadRaziei/tree/master/");
+
+exit();
+
+?>
+
+
